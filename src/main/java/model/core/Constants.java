91c935273c75e5975330a1a8cc604bea60f4b4d9
Uilen1 <42755635+Uilen1@users.noreply.github.com>
2020-05-25 22:31:08 -0300

    Update Constants.java

--- src/main/java/model/core/Constants.java
+++ src/main/java/model/core/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static String CABECALHO = "======================================== RUN_TEST ========================================\n\n";
 	public static String RODAPE = "======================================== END_TEST ========================================\n\n";
 	public static String DIRETORIO_RAIZ = "[PROJECT_DIRECTORY] - "+System.getProperty("user.dir");
-	public static String EMAIL = "uilenlelles@hotmail.com";
-	public static String SENHA = "72598757*";
+	public static String EMAIL = "teste@hotmail.com";
+	public static String SENHA = "teste";
 
 }
