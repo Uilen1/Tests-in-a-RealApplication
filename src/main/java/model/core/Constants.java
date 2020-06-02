16c5bf4d71f0f4999bb968921f83ca5afc2d7b35
Uilen Lelles <uilen.lelles.moreira@everis.com>
2020-06-01 23:24:48 -0300

    update value in package constants

--- src/main/java/model/core/Constants.java
+++ src/main/java/model/core/Constants.java
@@ -5,9 +5,8 @@ public class Constants {
 	public static String CABECALHO = "======================================== RUN_TEST ========================================\n\n";
 	public static String RODAPE = "======================================== END_TEST ========================================\n\n";
 	public static String DIRETORIO_RAIZ = "[PROJECT_DIRECTORY] - "+System.getProperty("user.dir");
-//	public static String EMAIL = "teste@hotmail.com";
-//	public static String SENHA = "teste";
-	public static String EMAIL = "uilenlelles@hotmail.com";
-	public static String SENHA = "72598757*";
+	public static String EMAIL = "teste@hotmail.com";
+	public static String SENHA = "teste";
+	
 
 }
