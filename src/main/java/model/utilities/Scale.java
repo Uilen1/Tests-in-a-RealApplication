a93f82b8b067d8f28336ee9f7bf4956a70250bca
Uilen1 <uilenmagnata@hotmail.com>
2021-03-23 16:38:32 -0300

    Corrigindo inicialização do driver e obtenção de escala da tela

--- src/main/java/model/utilities/Scale.java
+++ src/main/java/model/utilities/Scale.java
@@ -9,14 +9,15 @@ public class Scale {
 	
 	public static double getScreenScale() throws Exception {
 		WinDef.HDC hdc = GDI32.INSTANCE.CreateCompatibleDC(null);
-		if (hdc != null) {
-			float actual = GDI32.INSTANCE.GetDeviceCaps(hdc, 10 /* VERTRES */);
-			float logical = GDI32.INSTANCE.GetDeviceCaps(hdc, 117 /* DESKTOPVERTRES */);
-			GDI32.INSTANCE.DeleteDC(hdc);
-			if (logical != 0 && logical / actual >= 1) {
-				return logical / actual;
-			}
-		}
+//		if (hdc != null) {
+//			float actual = GDI32.INSTANCE.GetDeviceCaps(hdc, 10 /* VERTRES */);
+//			float logical = GDI32.INSTANCE.GetDeviceCaps(hdc, 117 /* DESKTOPVERTRES */);
+//			GDI32.INSTANCE.DeleteDC(hdc);
+//			if (logical != 0 && logical / actual >= 1) {
+//				return logical / actual;
+//			}
+//		}
+		//escala usa 96 como referencia para tela de 100%
 		return (Toolkit.getDefaultToolkit().getScreenResolution() / 96.0f);
 	}
 
